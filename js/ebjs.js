d72e5469d75c8d21fcfa4ed843c162cccedb7506
estebanbech <ebdesarrolloweb@gmail.com>
2022-10-23 17:58:51 -0300

    siguiendo con js

---
+++ js/ebjs.js
@@ -0,0 +1,6 @@
+let inputNombre = getElementById('name');
+let inputApellido = getElementById('lastname');
+let inputEmail = getElementById('email');
+let inputCantidad = getElementById('quantity');
+let inputCategoria = getElementById('category');
+let spanResultado = getElementById('resultadoSeleccion');
